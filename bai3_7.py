d4d13c4f6b2a6519a6f5b0100098c11fff7a7e76
Phúc họ phạm <phuchopham232@gmail.com>
2025-04-11 08:41:54 +0700

    Delete bai3_7.py

--- bai3_7.py
+++
@@ -1,8 +0,0 @@
-def checkValue(n):
-    if n%2==0:
-        print("Đây là số chẵn")
-    else:
-        print("Đây là số lẻ")
-checkValue(7)
-print("Sinh viên: Phạm Trọng Phúc")
-print(" MSSV: 235752020710029")
