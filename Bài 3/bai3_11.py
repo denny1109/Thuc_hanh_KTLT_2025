053ff8ab6e4f48ff79ba97e845dd67f4e4fa397e
Phúc họ phạm <phuchopham232@gmail.com>
2025-04-25 08:53:45 +0700

    Add files via upload

--- Bài 3/bai3_11.py
+++ Bài 3/bai3_11.py
@@ -1,16 +1,18 @@
 print("Phạm Trọng Phúc")
 print("MSSV:235752021710029")
-
+"""hàm tính tham số hàm này nhận 3 tham số t: lãi suất mỗi tháng (theo phần trăm),n: số vốn ban đầu,
+k: số tháng gửi tiết kiệm"""
 def benefit(t, n, k):
     # Tính số tiền nhận được sau k tháng
     total_amount = n * ((1 + t / 100) ** k)
     return total_amount
 
 # Nhập lãi suất, số vốn và số tháng
-t = float(input("Nhập lãi suất tiết kiệm (t%/tháng): "))
-n = float(input("Nhập số vốn ban đầu (n): "))
-k = int(input("Nhập số tháng gửi (k): "))
+t = float(input("Nhập lãi suất tiết kiệm (t%/tháng): "))#Nhập lãi suất (t): số thực
+n = float(input("Nhập số vốn ban đầu (n): "))#Nhập số vốn ban đầu (n): số thực
+k = int(input("Nhập số tháng gửi (k): "))#Nhập số tháng gửi (k): số nguyên
 
-# Tính số tiền nhận được
-result = benefit(t, n, k)
-print(f"Số tiền nhận được sau {k} tháng là: {result:.2f}")
+result = benefit(t, n, k)#Gọi hàm benefit(t, n, k) để tính tổng tiền nhận được.
+print(f"Số tiền nhận được sau {k} tháng là: {result:.2f}") #In kết quả với 2 chữ số thập phân (dùng :.2f để làm tròn)
+
+
--- Bài 3/bai3_9.py
+++ Bài 3/bai3_9.py
@@ -3,8 +3,8 @@ print("MSSV:23575202710029")
 
 """ Chương trình hoạt động như một cái máy tính bỏ túi bình thường  có thể cộng trù nhân chia """
 # This function adds two numbers  
-def tong(x, y): 
- return x + y 
+def tong(x, y): #Đây là hàm cộng hai số x và y.
+ return x + y #return trả lại kết quả phép cộng
 # This function subtracts two numbers  
 def hieu(x, y): 
  return x - y 
@@ -12,17 +12,21 @@ def hieu(x, y):
 def tich(x, y): 
  return x * y 
 # This function divides two numbers 
-def thuong(x, y): 
+def thuong(x, y): #Nếu y = 0 sẽ gây lỗi chia cho 0 — chương trình hiện tại chưa xử lý lỗi này
  return x / y 
+"""In ra hướng dẫn để người dùng chọn phép tính"""
 print("Select operation.") 
 print("1.tong") 
 print("2.hieu") 
 print("3.tich") 
 print("4.thuong") 
 # Take input from the user  
-choice = input("Enter choice(1/2/3/4):") 
+choice = input("Enter choice(1/2/3/4):") #Nhập số lựa chọn từ người dùng để xác định phép tính
+#Nhập hai số nguyên (int) từ người dùng để thực hiện phép tính
 num1 = int(input("Vui lòng nhập số đầu tiên: ")) 
 num2 = int(input("Vui lòng nhập số thứ hai: ")) 
+"""Kiểm tra biến choice để xác định phép tính tương ứng,Nếu nhập sai lựa chọn (choice không phải 1–4), 
+chương trình in:Invalid input"""
 if choice == '1': 
  print(num1,"+",num2,"=", tong(num1,num2)) 
 elif choice == '2': 
